cb829cda5b353149f7284e95873868bd684b4538
karenAF <karenjxliu@hotmail.com>
2017-04-30 13:02:39 -0500

    test association bt meal and restaurant

--- spec/models/meals_model_spec.rb
+++ spec/models/meals_model_spec.rb
@@ -6,7 +6,10 @@ RSpec.describe Meal, type: :model do
     expect(meal.restaurant_id).to eq(1)
     expect(meal.name).to eq("Fried Rice")
     expect(meal.price).to eq(2.49)
-    expect(meal.available).to eq(true)
+    expect(meal.available).to eq(true)  
+  end
+  it "should belong to restaurant" do
+    Meal.reflect_on_association(:restaurant).macro.should  eq(:belongs_to)
   end
   # it "belongs to restaurant" do
   #   meal = Meal.create!(restaurant_id: 1, name: "Fried Rice", price: 2.49, available: true)
--- spec/models/restaurants_model_spec.rb
+++ spec/models/restaurants_model_spec.rb
@@ -5,9 +5,13 @@ RSpec.describe Restaurant, type: :model do
     restaurant = Restaurant.create!(name: "Wong's Palace")
     expect(restaurant.name).to eq("Wong's Palace")
   end
+  it "should have many meals" do
+    Restaurant.reflect_on_association(:meals).macro.should == :has_many
+  end
+end
   # it "has many meals" do
   #   meal = Meal.create!(restaurant_id: 1, name: "Fried Rice", price: 2.49, available: true)
   #   restaurant = Restaurant.create!(name: "Wong's Palace")
   #   expect(restaurant.meals.first.name).to eq("Fried Rice")
   # end
-end
+# end
