91f0d6d67ea12fd3f8f038534e3141d39ec859e7
karenAF <karenjxliu@hotmail.com>
2017-04-30 12:55:16 -0500

    tried to test associations

--- spec/models/meals_model_spec.rb
+++ spec/models/meals_model_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Meal, type: :model do
   it "creates a new instance of Meal" do
-    meal = Meal.create!(restaurantid: 1, name: "Fried Rice", price: 2.49, available: true)
-    expect(meal.restaurantid).to eq(1)
+    meal = Meal.create!(restaurant_id: 1, name: "Fried Rice", price: 2.49, available: true)
+    expect(meal.restaurant_id).to eq(1)
     expect(meal.name).to eq("Fried Rice")
     expect(meal.price).to eq(2.49)
     expect(meal.available).to eq(true)
   end
+  # it "belongs to restaurant" do
+  #   meal = Meal.create!(restaurant_id: 1, name: "Fried Rice", price: 2.49, available: true)
+  #   restaurant = Restaurant.create!(name: "Wong's Palace")
+  #   expect(meal.restaurant.name).to eq("Wong's Palace")    
+  # end
 end
--- spec/models/restaurants_model_spec.rb
+++ spec/models/restaurants_model_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Restaurant, type: :model do
     restaurant = Restaurant.create!(name: "Wong's Palace")
     expect(restaurant.name).to eq("Wong's Palace")
   end
+  # it "has many meals" do
+  #   meal = Meal.create!(restaurant_id: 1, name: "Fried Rice", price: 2.49, available: true)
+  #   restaurant = Restaurant.create!(name: "Wong's Palace")
+  #   expect(restaurant.meals.first.name).to eq("Fried Rice")
+  # end
 end
