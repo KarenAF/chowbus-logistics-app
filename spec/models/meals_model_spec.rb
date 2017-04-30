638c5a89324ab7e12eb39f3b23e5630b01732d7d
karenAF <karenjxliu@hotmail.com>
2017-04-30 12:43:32 -0500

    fixed restaurant id column for meals

--- spec/models/meals_model_spec.rb
+++ spec/models/meals_model_spec.rb
@@ -1,9 +1,11 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Meal, type: :model do
-#   it { is_expected.to validate_presence_of(:name) } do
-#     Meal.new(restaurantid: 1, name: "Bokchoy", price: 0.3e1, available: true)
-#   end
-#   it { is_expected.to have_many(:users) }
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Meal, type: :model do
+  it "creates a new instance of Meal" do
+    meal = Meal.create!(restaurantid: 1, name: "Fried Rice", price: 2.49, available: true)
+    expect(meal.restaurantid).to eq(1)
+    expect(meal.name).to eq("Fried Rice")
+    expect(meal.price).to eq(2.49)
+    expect(meal.available).to eq(true)
+  end
+end
