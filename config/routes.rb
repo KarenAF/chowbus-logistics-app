f32c812060b1577cedbafe81239376470a8a9926
karenAF <karenjxliu@hotmail.com>
2017-05-01 17:06:02 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/' => 'meals#index'
+
+  get '/meals' => 'meals#index'
+  get '/meals/new' => 'meals#new'
+  post '/meals' => 'meals#create'
+  get '/meals/:id' => 'meals#show'
+
+  get '/test' => 'meals#test' # I used this to test the angular
+
+  namespace :api do
+    namespace :v1 do
+      get '/meals' => 'meals#index'
+      post '/meals' => 'meals#create'
+    end
+  end
+
+
+
 end
