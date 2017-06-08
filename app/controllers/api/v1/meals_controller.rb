9e5096bf14082f6823b915de3a18242c583693f4
karenAF <karenjxliu@hotmail.com>
2017-06-07 21:25:49 -0500

    added api controller code

--- app/controllers/api/v1/meals_controller.rb
+++ app/controllers/api/v1/meals_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::MealsController < ApplicationController
+ def index
+    @zone_schedules = ZoneSchedule.where(weekday: 1)
+    @meals = []
+    @zone_schedules.each do |zone_schedule|
+      zone_schedule.restaurant_id
+      # restaurant = Restaurant.find_by(id: zone_schedule.restaurant_id)
+      @meals << Meal.where(restaurant_id: zone_schedule.restaurant.id)
+      @meals.flatten!
+    end
+    # @meals = Meal.where()
+    render 'index.json.jbuilder'
+
+  end
 end
